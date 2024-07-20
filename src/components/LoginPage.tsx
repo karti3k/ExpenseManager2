b0c3a0bcae6100a5cc8ac55d561f962bbe4cfbb9
Kartik <kartikbhatia2304@gmail.com>
2024-07-20 13:21:07 +0530

    made responsive for mobile and tablet

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import LoginBgImg from '@/assets/LoginBgImg.png';
 import LoginPageImg from '@/assets/LoginPageImg.png';
-import AppIcon from '@/assets/AppIcon.png'
-import GoogleIcon from '@/assets/google-icon.png';
+import AppIcon from '@/assets/AppIcon.png';
+import EmailIcon from '@/assets/email_icon.svg';
+import UsernameIcon from '@/assets/username_icon.svg';
+import PasswordIcon from '@/assets/password_icon.svg';
+// import GoogleIcon from '@/assets/google-icon.png'; //we will use this later
 
 const LoginPage: React.FC = () => {
     const [isSignUp, setIsSignUp] = useState(false);
@@ -13,16 +16,18 @@ const LoginPage: React.FC = () => {
             <Image src={LoginBgImg} alt="Login Image" layout="fill" className="object-cover" />
 
             <div className="absolute inset-0 flex">
-                <div className="w-1/2 flex felx-col h-full bg-black bg-opacity-90 p-8 shadow-lg">
-                    <div className='w-full px-8 flex flex-col items-center justify-evenly'><Image src={AppIcon} alt='AppIcon' width={70}>
-                        </Image><Image src={LoginPageImg} alt='login-page-icon' width={350} height={50} className='pr-12 pb-12 drop-shadow-lg' ></Image>
-                        <div className='flex flex-col justify-center items-center gap-4'><h2 className='text-5xl font-bold text-stroke'>Hello User!</h2>
-                            <h3 className='text-xl font-bold text-center'>Manage your expenses easily with our simple free-to-use web-app.</h3>
+                <div className="hidden lg:flex lg:w-1/2 h-full bg-black bg-opacity-90 p-8 shadow-lg">
+                    <div className='w-full px-8 flex flex-col items-center justify-evenly'>
+                        <Image src={AppIcon} alt='AppIcon' width={70} />
+                        <Image src={LoginPageImg} alt='login-page-icon' width={350} height={50} className='pr-12 pb-12 drop-shadow-lg' />
+                        <div className='flex flex-col justify-center items-center gap-4'>
+                            <h2 className='text-5xl font-bold text-stroke'>Hello User!</h2>
+                            <h3 className='text-xl font-bold text-center'>Manage your expenses easily with this web-app.</h3>
                         </div>
                     </div>
                 </div>
-                <div className="w-1/2 h-full bg-custom-sky-blue bg-opacity-80 p-8 shadow-lg flex justify-center items-center">
-                    <div className="w-2/3 border-4 border-[#3997CB] bg-white p-8 rounded-3xl shadow-lg">
+                <div className="w-full lg:w-1/2 h-full bg-custom-sky-blue bg-opacity-80 p-8 shadow-lg flex justify-center items-center">
+                    <div className="sm:w-3/4 md:w-[55%] lg:w-2/3 border-4 border-[#3997CB] bg-white p-8 rounded-3xl shadow-lg">
                         <div className="flex justify-center gap-2 mb-4">
                             <button
                                 className={`px-4 py-2 ${!isSignUp ? 'text-custom-blueshade font-medium border-b-2 border-custom-blueshade' : 'text-custom-darkgray font-medium'}`}
@@ -38,23 +43,31 @@ const LoginPage: React.FC = () => {
                             </button>
                         </div>
 
-                        <button className="w-full border rounded-3xl border-custom-darkgray text-custom-darkgray flex justify-center mb-4 py-2 gap-3"><Image src={GoogleIcon} alt='GoogleIcon' width={25}></Image> <p className='font-semibold'>Continue with Google</p></button>
-
-                        <p className="text-custom-darkgray text-center mb-4">or connect with</p>
-
                         <form>
-                            <div className="mb-4">
+                            {isSignUp && (
+                                <div className="mb-4 relative">
+                                    <Image src={EmailIcon} alt="Email Icon" className="absolute left-3 top-2.5 w-5 h-5 icon-filter-gray" />
+                                    <input
+                                        type="email"
+                                        placeholder="Email"
+                                        className="w-full py-2 pl-10 pr-4 bg-custom-lightgray text-custom-darkgray font-semibold border border-gray-300 rounded-3xl"
+                                    />
+                                </div>
+                            )}
+                            <div className="mb-4 relative">
+                                <Image src={UsernameIcon} alt="Username Icon" className="absolute left-3 top-2.5 w-5 h-5 icon-filter-gray" />
                                 <input
-                                    type="email"
-                                    placeholder="Email"
-                                    className="w-full py-2 px-4 bg-custom-lightgray border border-gray-300 rounded-3xl"
+                                    type="text"
+                                    placeholder="Username"
+                                    className="w-full py-2 pl-10 pr-4 bg-custom-lightgray text-custom-darkgray font-semibold border border-gray-300 rounded-3xl"
                                 />
                             </div>
-                            <div className="mb-4">
+                            <div className="mb-4 relative">
+                                <Image src={PasswordIcon} alt="Password Icon" className="absolute left-3 top-2.5 w-5 h-5 icon-filter-gray" />
                                 <input
                                     type="password"
                                     placeholder="Password"
-                                    className="w-full py-2 px-4 bg-custom-lightgray border border-gray-300 rounded-3xl"
+                                    className="w-full py-2 pl-10 pr-4 bg-custom-lightgray text-custom-darkgray font-semibold border border-gray-300 rounded-3xl"
                                 />
                             </div>
 
@@ -67,9 +80,9 @@ const LoginPage: React.FC = () => {
                                 <div className="flex justify-between items-center mb-4">
                                     <label className="flex items-center">
                                         <input type="checkbox" className="mr-2" />
-                                       <p className='text-custom-darkgray'>Remember me</p> 
+                                        <p className='text-custom-darkgray lg:text-normal text-xs'>Remember me</p>
                                     </label>
-                                    <a href="#" className="text-custom-blueshade">Forgot password?</a>
+                                    <a href="#" className="text-custom-blueshade lg:text-normal text-xs">Forgot password?</a>
                                 </div>
                             )}
 
