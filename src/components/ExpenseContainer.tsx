e4793ccc18e792606ee36a4c8c701ba92d242c38
Kartik <kartikbhatia2304@gmail.com>
2024-07-30 20:24:08 +0530

    added buttons for switching between expenses and graph!

--- src/components/ExpenseContainer.tsx
+++ src/components/ExpenseContainer.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import Image from 'next/image'
+import ExpensesBttnIcon from '@/assets/expenses-bttn-icon.svg'
+import GraphBttnIcon from '@/assets/graph_bttn-icon.svg'
 const ExpenseContainer = () => {
   return (
-    <div className='w-full h-screen bg-[#E9F7FF] px-28 flex items-end '>
-        <div className='bg-white w-full h-[68%] rounded-t-3xl shadow-inner-custom'>
-
+    <div className='w-full h-screen bg-[#E9F7FF] px-28 flex flex-col justify-end'>
+        <div className='bg-white w-full h-[68%] rounded-3xl shadow-inner-custom flex flex-col justify-end'>
+            <div className='w-full h-12 bg-[#E9F7FF] rounded-xl flex'>
+                <button className='hover:font-bold flex gap-2 justify-center items-center w-1/2 h-12 border-t border-custom-sky-blue button-linear-gradient rounded-l-xl drop-shadow-2xl button-inner-shadow font-poppins text-white text-lg'><Image src={ExpensesBttnIcon} alt='E-icon' width={35} height={35}></Image> Expenses</button>
+                <button className='hover:font-bold flex gap-2 justify-center items-center w-1/2 h-12 border-t border-custom-sky-blue button-linear-gradient rounded-r-xl drop-shadow-2xl button-inner-shadow font-poppins text-white text-lg'><Image src={GraphBttnIcon} alt='G-icon' width={25} height={25}></Image> Graph</button>
+            </div>
         </div>
     </div>
   )
