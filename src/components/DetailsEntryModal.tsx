5b9564c1c10dfdca59b1685eac3ed65a31abe397
Kartik <kartikbhatia2304@gmail.com>
2024-08-17 13:16:11 +0530

    updated close logic for modal and changed its style according to the design

--- src/components/DetailsEntryModal.tsx
+++ src/components/DetailsEntryModal.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
-import AddCategoryicon from '@/assets/add_category_icon.svg'
-import AddImagesIcon from '@/assets/AddImageIcon.svg'
+import React, { useState, useEffect, useRef } from 'react';
+import AddCategoryicon from '@/assets/add_category_icon.svg';
+import AddImagesIcon from '@/assets/AddImageIcon.svg';
+import CloseIcon from '@/assets/CloseIcon.svg';
 import Image from 'next/image';
 
-const DetailsEntryModal: React.FC = () => {
+interface DetailsEntryModalProps {
+  onClose: () => void;
+}
+
+const DetailsEntryModal: React.FC<DetailsEntryModalProps> = ({ onClose }) => {
   const [amount, setAmount] = useState('');
   const [date, setDate] = useState('');
   const [details, setDetails] = useState('');
+  const modalRef = useRef<HTMLDivElement>(null);
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAmount(e.target.value);
@@ -20,51 +26,84 @@ const DetailsEntryModal: React.FC = () => {
     setDetails(e.target.value);
   };
 
+  const handleClose = () => {
+    onClose(); // Call the parent's toggleModal function
+  };
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      onClose(); // Close the modal if clicked outside
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
   return (
     <>
-        <div className="w-1/2 fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center font-poppins">
-          <div className="bg-white border-2 border-custom-darkgray p-6 pb-4 pt-16 rounded-lg shadow-lg w-80">
+      <div className="w-1/2 fixed inset-0 flex justify-center items-center font-poppins ">
+        <div ref={modalRef} className="p-6 pb-4 pt-16 w-80 relative bg-[#d9d9d9]/40 rounded-lg shadow backdrop-blur-sm">
+          {/* Close Icon */}
+          <button onClick={handleClose} className="absolute top-6 left-6">
+            <Image src={CloseIcon} alt="Close" width={25} height={25} className='icon-filter-red'/>
+          </button>
 
-            <div className="mb-4">
-              <label className="block text-gray-700 hidden">Amount</label>
-              <input
-                type="number"
-                value={amount}
-                onChange={handleAmountChange}
-                className="w-full px-3 py-2 border rounded-lg text-gray-400"
-                placeholder='Enter amount in RS'
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700 hidden">Date</label>
-              <input
-                type="date"
-                value={date}
-                onChange={handleDateChange}
-                className="w-full px-3 py-2 border rounded-lg text-gray-400"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700 hidden">Details</label>
-              <textarea
-                value={details}
-                onChange={handleDetailsChange}
-                className="w-full px-3 py-1 border rounded-lg resize-none text-gray-400"
-                placeholder='Enter more details (optional)'
-              />
-            </div>
-
-            <div className="flex justify-around">
-              <button className="icon-filter-blue"><Image src={AddCategoryicon} alt='add category' width={30} height={30}></Image>
-              </button>
-              <button className="icon-filter-blue">
-                <Image src={AddImagesIcon} alt='add images' width={30} height={30}></Image>
-              </button>
-            </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 hidden">Amount</label>
+            <input
+              type="number"
+              value={amount}
+              onChange={handleAmountChange}
+              className="w-full px-3 py-2 border rounded-lg text-gray-400 shadow"
+              placeholder="Enter amount in RS"
+            />
           </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 hidden">Date</label>
+            <input
+              type="date"
+              value={date}
+              onChange={handleDateChange}
+              className="bg-[#f0f9cd] w-full px-3 py-2 border rounded-lg text-gray-400 shadow"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 hidden">Details</label>
+            <textarea
+              value={details}
+              onChange={handleDetailsChange}
+              className="bg-[#cdd4f9] w-full px-3 py-1 border rounded-lg resize-none text-gray-400 shadow"
+              placeholder="Enter more details (optional)"
+            />
+          </div>
+
+          <div className="flex justify-around">
+  <div className="relative inline-block group">
+    <button className="icon-filter-dark-bluish rounded-md shadow">
+      <Image src={AddCategoryicon} alt="add category" width={30} height={30} />
+    </button>
+    <span className="absolute right-[0.4px] transform -translate-x-1/2 mt-1 text-gray-700 text-xs bg-white px-1 py-1 rounded-md shadow-lg opacity-0 group-hover:opacity-80 transition-opacity duration-200">
+      Add Category
+    </span>
+  </div>
+
+  <div className="relative inline-block group">
+    <button className="icon-filter-dark-bluish rounded-md shadow">
+      <Image src={AddImagesIcon} alt="add images" width={30} height={30} />
+    </button>
+    <span className="absolute transform translate-x-[4.8px] mt-1 text-gray-700 text-xs bg-white px-2 py-1 rounded-md shadow-lg opacity-0 text-right group-hover:opacity-100 transition-opacity duration-200">
+      Add Images
+    </span>
+  </div>
+</div>
         </div>
+      </div>
     </>
   );
 };
--- src/components/ExpenseContainer.tsx
+++ src/components/ExpenseContainer.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import ExpensesBttnIcon from '@/assets/expenses-bttn-icon.svg'
-import GraphBttnIcon from '@/assets/graph_bttn-icon.svg'
-import PlusIcon from '@/assets/plus.svg'
-import CheckIcon from '@/assets/check.svg'
-import DetailsEntryModal from './DetailsEntryModal'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import ExpensesBttnIcon from '@/assets/expenses-bttn-icon.svg';
+import GraphBttnIcon from '@/assets/graph_bttn-icon.svg';
+import PlusIcon from '@/assets/plus.svg';
+import CheckIcon from '@/assets/check.svg';
+import DetailsEntryModal from './DetailsEntryModal';
 
 const ExpenseContainer = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,22 +15,29 @@ const ExpenseContainer = () => {
 
   return (
     <div className='w-full h-screen bg-[#E9F7FF] px-28 flex flex-col justify-end'>
-        <div className='bg-white w-full h-[68%] rounded-3xl shadow-inner-custom flex flex-col justify-end'>
-            <div className='w-full h-12 bg-[#E9F7FF] rounded-xl flex justify-center items-end'>
-                <button className='hover:brightness-110 flex gap-2 justify-center items-center w-1/2 h-12 border-t border-custom-sky-blue button-linear-gradient rounded-l-xl drop-shadow-2xl button-inner-shadow font-poppins text-white text-lg'><Image src={ExpensesBttnIcon} alt='E-icon' width={35} height={35}></Image> Expenses</button>
+      <div className='bg-white w-full h-[68%] rounded-3xl shadow-inner-custom flex flex-col justify-end'>
+        <div className='w-full h-12 bg-[#E9F7FF] rounded-xl flex justify-center items-end'>
+          <button className='hover:brightness-110 flex gap-2 justify-center items-center w-1/2 h-12 border-t border-custom-sky-blue button-linear-gradient rounded-l-xl drop-shadow-2xl button-inner-shadow font-poppins text-white text-lg'>
+            <Image src={ExpensesBttnIcon} alt='E-icon' width={35} height={35}></Image> Expenses
+          </button>
 
-                <button className='hover:bg-custom-blueshade opacity-80 mb-4 z-10 absolute mx-auto w-12 h-12 rounded-full bg-custom-green border-2 border-custom-green flex justify-center items-center' onClick={toggleModal}><Image className='w-full icon-filter-green ' src={isModalOpen ? CheckIcon : PlusIcon} 
-        alt={isModalOpen ? 'check' : 'add'}></Image></button>
+          <button className='hover:bg-custom-blueshade opacity-80 mb-4 z-10 absolute mx-auto w-12 h-12 rounded-full bg-custom-green border-2 border-custom-green flex justify-center items-center' onClick={toggleModal}>
+            <Image className='w-full icon-filter-green ' src={isModalOpen ? CheckIcon : PlusIcon} alt={isModalOpen ? 'check' : 'add'}></Image>
+          </button>
 
-                <button className='hover:brightness-110 flex gap-2 justify-center items-center w-1/2 h-12 border-t border-custom-sky-blue button-linear-gradient rounded-r-xl drop-shadow-2xl button-inner-shadow font-poppins text-white text-lg'><Image src={GraphBttnIcon} alt='G-icon' width={25} height={25}></Image> Graph</button>
-            </div>
-
-            {isModalOpen && (<div className='w-1/2 h-8 bottom-36 absolute left-[62.2%] transform -translate-x-1/2'>
-            <DetailsEntryModal />
-          </div>)}
+          <button className='hover:brightness-110 flex gap-2 justify-center items-center w-1/2 h-12 border-t border-custom-sky-blue button-linear-gradient rounded-r-xl drop-shadow-2xl button-inner-shadow font-poppins text-white text-lg'>
+            <Image src={GraphBttnIcon} alt='G-icon' width={25} height={25}></Image> Graph
+          </button>
         </div>
-    </div>
-  )
-}
 
-export default ExpenseContainer
+        {isModalOpen && (
+          <div className='w-1/2 h-8 bottom-36 absolute left-[62.2%] transform -translate-x-1/2'>
+            <DetailsEntryModal onClose={toggleModal} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseContainer;
