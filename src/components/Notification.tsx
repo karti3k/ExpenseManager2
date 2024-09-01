f4679043312845bfd86c52d40ed4245ce36d9997
Kartik <kartikbhatia2304@gmail.com>
2024-09-01 12:00:27 +0530

    finalized notification theme for light theme

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -21,7 +21,7 @@ const Notification: React.FC<NotificationProps> = ({ message, type, onClose }) =
 
     return (
          <div className={`fixed top-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 max-w-sm w-[90%] p-4 rounded-lg shadow-lg
-            ${type === 'success' ? 'bg-green-700 border-2 border-custom-light-green text-custom-light-green' : 'text-red-200 bg-red-500 border-2 border-red-200'}`}>
+            ${type === 'success' ? 'bg-white border border-black text-[#00ffa0]' : 'text-red-500 bg-white border border-black'}`}>
             <div className="flex justify-between items-center">
                 <p className="text-sm font-medium">{message}</p>
                 <button onClick={onClose} className="ml-4 text-xl font-bold">
